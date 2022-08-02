f0d34b5c3d8273a19c36a0181b4e3e8c12d67fd0
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 14:42:37 +0530

    Finding Quotient and Reminder

---
+++ QuotientAndReminder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public static class QuotientAndReminder
+    {
+        public static void FindQuotientAndReminder()
+        {
+            int quoteint, reminder;
+            Console.WriteLine("Enter the Divident: ");
+            int divident = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            quoteint = divident / divisor;
+            Console.WriteLine("Quoteint is: " + quoteint);
+
+            reminder = divident % divisor;
+            Console.WriteLine("Remider is: " + reminder);
+
+        }
+    }
+}
