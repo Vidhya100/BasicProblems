1aeda27a41eb0d06882e16e040c5a0742f2e8966
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 14:36:53 +0530

    Harmonic series calculated

---
+++ HarmonicSeries.cs
@@ -0,0 +1,26 @@
+﻿namespace BasicPrograms
+{
+    public class HarmonicSeries
+    {
+        public static void FindHarmonicSeries()
+        {
+            double H = 1;
+            Console.WriteLine("Enter the number to print harmonic series ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N != 0)
+            {
+                for (int i = 2; i <= N; i++)
+                {
+                    H += 1 / (double)i;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("give value other than 0 .");
+            }
+            Console.WriteLine("Harmonic value  " + H);
+        }
+    }
+}
