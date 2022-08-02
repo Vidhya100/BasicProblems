8d2d8336fc463e47491e732185662e52bea3f7aa
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 14:50:16 +0530

    Finding Vowel and Consonants.

---
+++ VowelOrConsonent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public static class VowelOrConsonent
+    {
+        public static void CheckVowelOrConsonent()
+        {
+            Console.WriteLine("Enter alphabet.");
+            string ch = Console.ReadLine();
+
+            if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u")
+            {
+                Console.WriteLine("Given {0} is Vowel.",ch);
+            }
+            else
+            {
+                Console.WriteLine("Given {0} is Consonent.", ch);
+            }
+        }
+    }
+}
