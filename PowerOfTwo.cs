2df681bf237d10d26474e848d0a30f060dc073bc
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 14:27:43 +0530

    Find power of two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicPrograms
+{
+    public class PowerOfTwo
+    {
+        public static void CalculatePowerOfTwo()
+        {
+            Console.WriteLine("Enter the power value: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0 && num <= 31)
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    double result = Math.Pow(2, i);
+                    Console.WriteLine("2 to the power of" + i + "is " + result);
+                }
+            }
+        }
+    }
+}
+    
+        
