95d6dd3979f4c4391eddc10018befd2754114b9c
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 12:42:21 +0530

    Program file added.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FlipCoin.CalculateFlipPercentage();
+            LeapYear.CheckLeapYear();
+            PowerOfTwo.CalculatePowerOfTwo();
+            HarmonicSeries.FindHarmonicSeries();
+            Factors.CalculateFactors();
+            QuotientAndReminder.FindQuotientAndReminder();
+            SwapNumbers.SwapTheNumber();
+            EvenOrOdd.CheckEvenOdd();
+            VowelOrConsonent.CheckVowelOrConsonent();
+            FindLargest.FindAmongLargest();
+        }
+    }
+}
