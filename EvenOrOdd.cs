50b05ddd9e39b10264a9ff7d7428f08cbda8ee44
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 14:47:02 +0530

    Finding Even or Odd Number

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+   public static class EvenOrOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Given number {0} is even.", num);
+            }
+            else
+            {
+                Console.WriteLine("Given number {0} is odd.", num);
+            }
+        }
+    }
+}
